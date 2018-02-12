02f46c36ae54e2fbfd5e924476cebc17d764f105
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-12 12:22:59 -0800

    add event listeners to controller

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,44 +1,66 @@
 /***********************************************/
 /*                CONTROLLER                   */
 /***********************************************/
+var controller = {
+  resetButton: document.getElementsByClassName('reset')[0],
+
+  squares: document.getElementsByClassName('square')
+};
+
 // reset button click: tell model to reset game
+controller.resetButton.addEventListener('click', (event) => {
+  model.resetGame();
+});
 
 // game square click: tell model to play the piece
+for (let square of controller.squares) {
+  square.addEventListener('click', (event) => {
+    model.playPiece(event.target);
+  })
+}
 
 
 /***********************************************/
 /*                   MODEL                     */
 /***********************************************/
-var game = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
-];
+var model = {
+  game: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ],
 
-// reset game board in storage
-// tell view to reset board on page
-var resetGame = () => {};
+  // reset game board in storage
+  // tell view to reset board on page
+  resetGame: () => {
+    console.log('reset clicked');
+  },
 
-// add piece to game board in storage
-// tell view to display piece on board on page
-var playPiece = (player, location) => {};
+  // add piece to game board in storage
+  // tell view to display piece on board on page
+  playPiece: (square) => {
+    console.log('play piece at ' + square.className);
+  },
 
-// check if there's a winner
-// tell view to display winner if yes
-var hasWon = () => {};
+  // check if there's a winner
+  // tell view to display winner if yes
+  hasWon: () => {},
 
-// check if there's a tie
-// tell view to display tie message if yes
-var hasTied = () => {};
+  // check if there's a tie
+  // tell view to display tie message if yes
+  hasTied: () => {}
+};
 
 
 /***********************************************/
 /*                    VIEW                     */
 /***********************************************/
-var displayPiece = (player, location) => {};
+var view = {
+  displayPiece: (player, location) => {},
 
-var freshBoard = () => {};
+  freshBoard: () => {},
 
-var switchPlayer = () => {};
+  switchPlayer: () => {},
 
-var endGameMessage = () => {};
+  endGameMessage: () => {}
+};
