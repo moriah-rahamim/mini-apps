0a958a6e312d57ab7cc41a1021bdc0339dc90f50
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-12 11:55:57 -0800

    add basic MVC scaffolding for app.js

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,44 @@
+/***********************************************/
+/*                CONTROLLER                   */
+/***********************************************/
+// reset button click: tell model to reset game
+
+// game square click: tell model to play the piece
 
 
+/***********************************************/
+/*                   MODEL                     */
+/***********************************************/
+var game = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+
+// reset game board in storage
+// tell view to reset board on page
+var resetGame = () => {};
+
+// add piece to game board in storage
+// tell view to display piece on board on page
+var playPiece = (player, location) => {};
+
+// check if there's a winner
+// tell view to display winner if yes
+var hasWon = () => {};
+
+// check if there's a tie
+// tell view to display tie message if yes
+var hasTied = () => {};
+
+
+/***********************************************/
+/*                    VIEW                     */
+/***********************************************/
+var displayPiece = (player, location) => {};
+
+var freshBoard = () => {};
+
+var switchPlayer = () => {};
+
+var endGameMessage = () => {};
