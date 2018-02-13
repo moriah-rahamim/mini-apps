bf9458ea5ff8718eddf6388ee764315cf594fff3
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-13 12:10:23 -0800

    create app.js MVC scaffolding

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,75 @@
+var init = () => {
+  var model = new Model();
+  var controler = new Controller(model);
+  var view = new View(model);
+}
+
+// *****************************************************************************
+// CONTROLLER
+// *****************************************************************************
+class Controller {
+
+  constructor(model) {
+    this.$form = $('form');
+    this.$inputBox = $('.json-input');
+    this.$submitButton = $('.submit');
+    this.model = model;
+
+    this.$submitButton.on('click', this.submitHandler.bind(this));
+    this.$form.on('submit', event => event.preventDefault());
+  }
+
+  submitHandler() {
+    let input = this.$inputBox.val();
+    this.$inputBox.val('');
+    this.model.input(input);
+  }
+}
+
+// *****************************************************************************
+// MODEL
+// *****************************************************************************
+class Model {
+
+  constructor() {
+    this.json = '';
+    this.csv = '';
+  }
+
+  // update this.json
+  // get a new csv version of the string
+  // update this.csv
+  input(json) {
+
+  }
+
+  setJson(json) {
+    
+  }
+
+  getCsv(json) {
+
+  }
+
+  setCsv(json) {
+
+  }
+}
 
 
+
+
+// *****************************************************************************
+// VIEW
+// *****************************************************************************
+class View {
+  constructor(model) {
+    this.model = model;
+    // variable to hold json container div
+    // variable to hold csv container div
+  }
+
+  render() {
+    
+  }
+}
