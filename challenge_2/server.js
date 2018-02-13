86adf5497c7a285921a4fed0efc94f5e141dc029
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-13 11:07:37 -0800

    create basic server structure and index.html page structure

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname,'client')));
 
+app.listen(8000, () => console.log('listening on localhost:8000!'))
