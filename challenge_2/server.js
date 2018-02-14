b813ece0ce8463c1cab372017ec4d22e1e782e6e
Moriah Rahamim <moriahfaye@gmail.com>
2018-02-13 16:22:52 -0800

    implement server model - convert object to csv

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,67 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(express.static(path.join(__dirname,'client')));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json());
 
-app.listen(8000, () => console.log('listening on localhost:8000!'))
+// *****************************************************************************
+// CONTROLLER
+// *****************************************************************************
+app.post('/', (request, response) => {
+  let csv = services.objToCsv(request.body); // already an object
+  response.send(csv);
+});
+
+// *****************************************************************************
+// MODEL
+// *****************************************************************************
+class Services {
+  constructor() {}
+
+  _getCsvHeaders(obj) {
+    let headers = [];
+    for (let key in obj) {
+      if (key !== 'children') {
+        headers.push(key);
+      }
+    }
+    return headers;
+  }
+
+  _getCsvData (obj, headers) {
+    var rowArr = [];
+
+    headers.forEach((colName) => {
+      rowArr.push(obj[colName]);
+    });
+    var csv = rowArr.join(',');
+
+    // if children, recurse and concat. else just return this row
+    if (obj.children) {
+      obj.children.forEach((child) => {
+        csv += '\n' + this._getCsvData(child, headers);
+      });
+    }
+    return csv;
+  }
+
+  objToCsv(obj) {
+    let headersArr = this._getCsvHeaders(obj);
+    let headersCsv = headersArr.join(',');
+    let bodyCsv = this._getCsvData(obj, headersArr);
+
+    let csvFull = headersCsv + '\n' + bodyCsv;
+
+    return(csvFull);
+  }
+};
+
+// *****************************************************************************
+// INIT
+// *****************************************************************************
+
+var services = new Services();
+app.listen(8000, () => console.log('listening on localhost:8000!'));
